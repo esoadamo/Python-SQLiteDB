4e4c0a2953c4b87623b2ac7c920e0ad242cadcb7
Adam Hlaváček <git@adamhlavacek.com>
2020-12-21 13:54:23 +0100

    + indexedb.py: save/load any jsonizable data type

--- sqlitedb/indexedb.py
+++ sqlitedb/indexedb.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 from random import choice
 from typing import Union, Optional, Callable, Dict, Any
@@ -24,14 +25,9 @@ class IndexedDBManager:
 class IndexedDB:
     def __init__(self, manager: IndexedDBManager, db_name: str):
         self.__manager = manager
+        self.__table_created = False
+        self.__supports_jsonization: Optional[bool] = None
         self.__table = "`db_%s`" % db_name.replace('`', '``')
-        manager.sqlitedb.execute(f"""
-        CREATE TABLE IF NOT EXISTS {self.__table} (
-        `id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-        `key`	TEXT NOT NULL UNIQUE,
-        `value`	TEXT
-        );
-        """)
 
     def get(self, item: str, default: Optional[Any] = None) -> Any:
         try:
@@ -43,21 +39,76 @@ class IndexedDB:
         return not not self.__manager.sqlitedb.execute(f"SELECT value FROM {self.__table} WHERE key=?", (item,))
 
     def __getitem__(self, item: str) -> Any:
-        r = self.__manager.sqlitedb.execute(f"SELECT value FROM {self.__table} WHERE key=?", (item,))
+        if self.__test_supports_jsonization():
+            cmd = f"SELECT value, jsonized FROM {self.__table} WHERE key=?"
+        else:
+            cmd = f"SELECT value FROM {self.__table} WHERE key=?"
+
+        r = self.__manager.sqlitedb.execute(cmd, (item,))
         if r:
-            return r[0][0]
+            value = r[0][0]
+
+            if self.__supports_jsonization and r[0][1]:
+                value = json.loads(value)
+
+            return value
         raise IndexError
 
     def __setitem__(self, key: str, value: Any) -> None:
+        jsonize = type(value) is not str
+
+        if jsonize:
+            self.__support_jsonization()
+            value = json.dumps(value)
+
         if self.key_exists(key):
-            self.__manager.sqlitedb.execute(f"UPDATE {self.__table} SET value=? WHERE key=?", (value, key))
+            if self.__test_supports_jsonization():
+                self.__manager.sqlitedb.execute(f"""
+                UPDATE {self.__table} SET value=?, jsonized=? WHERE key=?
+                """, (value, jsonize, key))
+            else:
+                self.__manager.sqlitedb.execute(f"UPDATE {self.__table} SET value=? WHERE key=?", (value, key))
         else:
-            self.__manager.sqlitedb.execute(
-                f"INSERT INTO {self.__table} (`key`,`value`) VALUES (?,?);", (key, value)
-            )
+            if self.__supports_jsonization:
+                self.__manager.sqlitedb.execute(
+                    f"INSERT INTO {self.__table} (`key`,`value`,`jsonized`) VALUES (?,?,?);", (key, value, jsonize)
+                )
+            else:
+                self.__manager.sqlitedb.execute(
+                    f"INSERT INTO {self.__table} (`key`,`value`) VALUES (?,?);", (key, value)
+                )
         self.__manager.sqlitedb.commit()
 
     def __delitem__(self, key: str) -> None:
         self.__manager.sqlitedb.execute(
             f"DELETE FROM {self.__table} WHERE `key`=?;", (key,)
         )
+
+    def __create_table(self) -> None:
+        if self.__table_created:
+            return
+        self.__manager.sqlitedb.execute(f"""
+        CREATE TABLE IF NOT EXISTS {self.__table} (
+        `key`	TEXT NOT NULL UNIQUE,
+        `value`	TEXT
+        );
+        """)
+        self.__table_created = True
+
+    def __test_supports_jsonization(self) -> bool:
+        if self.__supports_jsonization is not None:
+            return self.__supports_jsonization
+        for _, column_name, _, _, _, _ in self.__manager.sqlitedb.execute(f"PRAGMA table_info({self.__table})"):
+            if column_name == 'jsonized':
+                self.__supports_jsonization = True
+                return True
+        self.__supports_jsonization = False
+        return False
+
+    def __support_jsonization(self) -> None:
+        if self.__supports_jsonization or self.__test_supports_jsonization():
+            return
+        self.__manager.sqlitedb.execute(f"""
+        ALTER TABLE {self.__table} ADD COLUMN jsonized INTEGER NOT NULL DEFAULT 0
+        """)
+        self.__supports_jsonization = True
