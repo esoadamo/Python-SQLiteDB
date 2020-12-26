f2dc5ee0941595b6c5c3a5aac23d8b470d94a9f2
Adam Hlaváček <git@adamhlavacek.com>
2020-12-26 13:59:28 +0100

    ~ raise versiom

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = "None yet"
 
 setuptools.setup(
     name="sqlitedb",
-    version="0.9.3",
+    version="0.9.4",
     author="Adam Hlavacek",
     author_email="git@adamhlavacek.com",
     description="A thread-safe SQLite database",
